0360c562f5e19777bf788bb7e8d2eeab424bec42
alexandre.carvalho@live.com <alexandre.carvalho@live.com>
2020-04-07 10:38:37 -0300

    Creating the tables in the database

---
+++ AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstEfCoreApp.Entities;
+
+namespace MyFirstEfCoreApp
+{
+	public class AppDbContext : DbContext
+	{
+		private const string ConnectionString =
+			@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MyFirstEfCoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		
+		protected override void OnConfiguring(
+			DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(ConnectionString);
+		}
+
+		public DbSet<Book> Books { get; set; }
+	}
+}
---
+++ Entities/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstEfCoreApp.Entities
+{
+	public class Author
+	{
+		public int Authorid { get; set; }
+		public string Name { get; set; }
+		public string WebUrl { get; set; }
+	}
+}
